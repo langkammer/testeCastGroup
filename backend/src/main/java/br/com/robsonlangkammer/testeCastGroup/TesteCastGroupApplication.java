b45004ddc234f2e582651b32b7d47a01fda7cd0a
ROBSON UBUNTU CASAS <robsonlangkammer@hotmail.com>
2019-07-24 20:40:07 -0300

    ini do projeto

---
+++ backend/src/main/java/br/com/robsonlangkammer/testeCastGroup/TesteCastGroupApplication.java
@@ -0,0 +1,13 @@
+package br.com.robsonlangkammer.testeCastGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCastGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCastGroupApplication.class, args);
+	}
+
+}
